47c6d7c1d636c93460d0027b2312a94d8f50212c
Franco Ayala <franco.a98@outlook.com>
2023-03-01 13:06:51 -0300

    Initial version of blog generated by generator-jhipster@7.9.3

---
+++ src/main/webapp/app/entities/post/post.test-samples.ts
@@ -0,0 +1,36 @@
+import dayjs from 'dayjs/esm';
+
+import { IPost, NewPost } from './post.model';
+
+export const sampleWithRequiredData: IPost = {
+  id: 35989,
+  title: 'Fantastic',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2023-03-01T01:40'),
+};
+
+export const sampleWithPartialData: IPost = {
+  id: 93202,
+  title: 'Seychelles',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2023-02-28T17:53'),
+};
+
+export const sampleWithFullData: IPost = {
+  id: 23240,
+  title: 'recontextualize virtual copying',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2023-02-28T19:34'),
+};
+
+export const sampleWithNewData: NewPost = {
+  title: 'withdrawal',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2023-02-28T21:05'),
+  id: null,
+};
+
+Object.freeze(sampleWithNewData);
+Object.freeze(sampleWithRequiredData);
+Object.freeze(sampleWithPartialData);
+Object.freeze(sampleWithFullData);
